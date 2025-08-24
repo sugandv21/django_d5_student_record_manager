98943dbb8c391e7a6fdcbec51fedf063ec5a5ef3
sugandv21 <sdvsuganya@gmail.com>
2025-08-24 18:01:38 +0530

    Update settings.py

--- student_project/settings.py
+++ student_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3w9!d9z@e(#ao$s1sjvabha)7&!5t94b$cpcyxc!4auoic%1c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,3 +125,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
